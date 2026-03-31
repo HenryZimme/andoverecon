06ee40e3a82d90118792e2aa1f462baa7f3fcc29
Henry Zimmerman <henry.s.zimmerman@gmail.com>
2026-03-31 19:32:49 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -8,7 +8,7 @@
     { href: 'resources.html',   label: 'Resources' },
     { href: 'board.html',       label: 'Board' },
   ];
-henryzimmerman.github.io
+andoverecon.org
   const current = location.pathname.split('/').pop() || 'index.html';
 
   const linksHtml = pages.map(p =>
